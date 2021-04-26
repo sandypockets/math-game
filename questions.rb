d865f31d900bf8d36fdb9c7815c9d5ba2df66ec8
sandypockets <zac@znacreative.com>
2021-04-26 12:04:57 -0400

    Adjust random number values to meet outlined game requirements

--- questions.rb
+++ questions.rb
@@ -1,9 +1,9 @@
 class Questions
   def initialize
     ## First integer random number
-    @first_num = rand(10)
+    @first_num = rand(20)
     ## Second integer random number
-    @second_num = rand(10)
+    @second_num = rand(20)
     ## Answer to be compared
     @answer = @first_num + @second_num
     ## Question to be displayed to the player
