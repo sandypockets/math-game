e98155aa496cb53b8869872bc6d730b2814cab9f
sandypockets <zac@znacreative.com>
2021-04-26 12:19:11 -0400

    Add creation of new players using Players class

--- main.rb
+++ main.rb
@@ -10,4 +10,13 @@ puts 'Player 2, please enter your name... '
 player2_name = gets.chomp
 puts "Thank you #{player2_name}."
 
+# Create 2 new players from the Players class
+player1 = Players.new(player1_name)
+player2 = Players.new(player2_name)
+
+# Testing/Debugging
+puts player1.name
+puts player2.name
+
+
 
