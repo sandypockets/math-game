89b7805c020375d410fc418299cec427fc60ad5e
sandypockets <zac@znacreative.com>
2021-04-26 13:36:36 -0400

    Set up loop and lives tracking. Basic game working

--- main.rb
+++ main.rb
@@ -11,24 +11,50 @@ player2_name = gets.chomp
 puts "Thank you #{player2_name}."
 
 # Create 2 new players from the Players class
-player1 = Players.new(player1_name)
-player2 = Players.new(player2_name)
+@player1 = Players.new(player1_name)
+@player2 = Players.new(player2_name)
 
-# First question
-question = Questions.new
-puts question.question
-user_answer = gets.chomp
-
-# Convert question.answer to string to match user input then check answers
-if user_answer == question.answer.to_s
-  puts 'You got it right!'
-else
-  puts 'Incorrect!'
+def remove_life(player)
+  player.lives -= 1
+  puts player.lives
 end
 
-# Testing/Debugging
-puts player1.name
-puts player2.name
+def output_score
+  puts "--- #{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3 ---"
+end
+
+def check_answer(user_input, current_question, player)
+  ## Convert question.answer to string to match user input then check answers
+  if user_input == current_question.answer.to_s
+    puts 'You got it right!'
+  else
+    puts 'Incorrect!'
+    ## Remove a life from the player
+    remove_life(player)
+    puts player.lives
+  end
+end
+
+def ask_question(player)
+  question_one = Questions.new
+  puts "#{player.name}, you're up!"
+  puts question_one.question
+  user_answer = gets.chomp
+  check_answer(user_answer, question_one, player)
+  output_score
+end
+
+def ask_question_to_each_player
+  ## Player 1
+  ask_question(@player1)
+  ## Player 2
+  ask_question(@player2)
+  puts '--- NEW ROUND ---'
+end
+
+# Run the game as long as both players are still alive
+ask_question_to_each_player while @player1.still_alive? && @player2.still_alive?
+
 
 
 
