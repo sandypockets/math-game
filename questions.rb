2c5309bb0ac96b178b4a9ad88f5ae2966fa1505c
sandypockets <zac@znacreative.com>
2021-04-26 12:02:51 -0400

    Add @question and attr_accessor

--- questions.rb
+++ questions.rb
@@ -6,7 +6,10 @@ class Questions
     @second_num = rand(10)
     ## Answer to be compared
     @answer = @first_num + @second_num
+    ## Question to be displayed to the player
+    @question = "What is the sum of #{@first_num} and #{@second_num}?"
   end
+  attr_accessor :answer, :question
 
 end
 
