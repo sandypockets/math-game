a12e2dee7fb49a688ed1482ea24656ebf0c4e1fd
sandypockets <zac@znacreative.com>
2021-04-26 12:15:15 -0400

    Fix typo in require filepath

---
+++ main.rb
@@ -0,0 +1,13 @@
+require './players'
+require './questions'
+
+# Welcome message
+puts 'Welcome to the two player Math Game!'
+puts 'Player 1, please enter your name... '
+player1_name = gets.chomp
+puts "Thank you #{player1_name}."
+puts 'Player 2, please enter your name... '
+player2_name = gets.chomp
+puts "Thank you #{player2_name}."
+
+
